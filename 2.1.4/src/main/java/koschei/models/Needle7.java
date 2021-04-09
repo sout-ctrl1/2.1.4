7e4b9559f07598eac7e140ee9345a98fb2a1b055
sout-ctrl1 <75034017+sout-ctrl1@users.noreply.github.com>
2021-04-09 17:28:50 +0500

    Add files via upload

---
+++ 2.1.4/src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,20 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+    Deth8 deth;
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + deth.toString();
+    }
+
+    @Autowired
+    public void setDeth(Deth8 deth) {
+        this.deth = deth;
+    }
+}
